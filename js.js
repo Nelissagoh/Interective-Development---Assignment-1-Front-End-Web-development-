3158e3c8047709db18b7f8f5bcddf818d198dad9
S10244321F <s10244321@connect.np.edu.sg>
2022-12-04 18:14:18 +0800

    fix error according to validator

--- js.js
+++ js.js
@@ -1,12 +1,4 @@
-var navL= document.getElementById("navL")
 
-function openmenu(){
-    navL.style.right="0";
-}
-
-function closemenu(){
-    navL.style.right="-200px";
-}
 
 // Automatic Slideshow - change image every 3 seconds
 var myIndex = 0;
